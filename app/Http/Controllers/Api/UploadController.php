6e9e6621f126a722b9fe28040b2a79c6bfafe2e6
Marosdee Uma <danya0365@gmail.com>
2023-06-15 09:49:19 +0700

    refactor: implement upload file trait

--- app/Http/Controllers/Api/UploadController.php
+++ app/Http/Controllers/Api/UploadController.php
@@ -3,138 +3,30 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Traits\UploadTrait;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
-use Intervention\Image\Facades\Image;
-use Illuminate\Support\Str;
 
 class UploadController extends Controller
 {
-    public function uploadPhoto(Request $request)
+    use UploadTrait;
+
+    public function doUploadPhoto(Request $request)
     {
-        $uploadName = 'photo';
-        $validatedRequest = Validator::make(
-            $request->all(),
-            [
-                $uploadName => 'required|image|max:2048',
-            ]
-        );
-
-        if ($validatedRequest->fails()) {
-            return response()->json([
-                'status' => false,
-                'message' => 'validation error',
-                'errors' => $validatedRequest->errors()
-            ], 401);
-        }
-
-        if (!$request->hasFile($uploadName) || !$request->file($uploadName)->isValid()) {
-            return response()->json([
-                'status' => false,
-                'message' => 'validation error',
-                'errors' => 'file not found'
-            ], 401);
-        }
-
-        try {
-            $photo = $request->file($uploadName);
-            //$fileName = $photo->getClientOriginalName();
-            //$fileName = str_replace(' ', '_', $fileName);
-            $photoExtension = $photo->getClientOriginalExtension();
-            $originalFileName = 'original.' . $photoExtension;
-            $resizeFileName = 'resize_w800.' . $photoExtension;
-
-            $date = \Carbon\Carbon::now()->format('Y-m-d');
-            $random = Str::random(6);
-            $storeOriginalFileDir = "$date/$random/$originalFileName";
-            $storeResizeFileDir = "$date/$random/$resizeFileName";
-            $photoPath = $photo->storeAs('images', $storeOriginalFileDir);
-
-            $resizedPhoto = Image::make($photoPath)
-                ->resize(2000, null, function ($constraint) {
-                    $constraint->aspectRatio();
-                    $constraint->upsize();
-                })
-                ->save();
-
-            $resizedPhoto = Image::make($photoPath)
-                ->resize(800, null, function ($constraint) {
-                    $constraint->aspectRatio();
-                    $constraint->upsize();
-                })
-                ->save('images/' . $storeResizeFileDir);
-
-            return response()->json([
-                'status' => true,
-                'message' => 'Photo uploaded successfully',
-                'data' => [
-                    'original' => asset($photoPath),
-                    'resize' => asset('images/' . $storeResizeFileDir)
-                ],
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], 500);
+        $uploadResult = $this->uploadPhoto($request, 'photo');
+        if ($uploadResult['status']) {
+            return response()->json($uploadResult, 200);
+        } else {
+            return response()->json($uploadResult, 500);
         }
     }
 
-    public function uploadAvatar(Request $request)
+    public function doUploadAvatar(Request $request)
     {
-        $uploadName = 'photo';
-        $validatedRequest = Validator::make(
-            $request->all(),
-            [
-                $uploadName => 'required|image|max:2048',
-            ]
-        );
-
-        if ($validatedRequest->fails()) {
-            return response()->json([
-                'status' => false,
-                'message' => 'validation error',
-                'errors' => $validatedRequest->errors()
-            ], 401);
-        }
-
-        if (!$request->hasFile($uploadName) || !$request->file($uploadName)->isValid()) {
-            return response()->json([
-                'status' => false,
-                'message' => 'validation error',
-                'errors' => 'file not found'
-            ], 401);
-        }
-
-        try {
-            $photo = $request->file($uploadName);
-            $photoExtension = $photo->getClientOriginalExtension();
-            $originalFileName = 'avatar.' . $photoExtension;
-
-            $date = \Carbon\Carbon::now()->format('Y-m-d');
-            $random = Str::random(6);
-            $storeOriginalFileDir = "$date/$random/$originalFileName";
-            $photoPath = $photo->storeAs('images', $storeOriginalFileDir);
-
-            $resizedPhoto = Image::make($photoPath)
-                ->resize(512, null, function ($constraint) {
-                    $constraint->aspectRatio();
-                    $constraint->upsize();
-                })
-                ->save();
-
-            return response()->json([
-                'status' => true,
-                'message' => 'Photo uploaded successfully',
-                'data' => [
-                    'avatar' => asset($photoPath),
-                ],
-            ], 200);
-        } catch (\Throwable $th) {
-            return response()->json([
-                'status' => false,
-                'message' => $th->getMessage()
-            ], 500);
+        $uploadResult = $this->uploadAvatar($request, 'photo');
+        if ($uploadResult['status']) {
+            return response()->json($uploadResult, 200);
+        } else {
+            return response()->json($uploadResult, 500);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::group(['middleware' => 'cors'], function () {
         Route::post('works', [WorkController::class, 'createWork']);
         Route::post('works/{workId}/likes', [WorkController::class, 'createWorkLike']);
         Route::post('recruits', [RecruitController::class, 'createRecruit']);
-        Route::post('upload/photo', [UploadController::class, 'uploadPhoto']);
-        Route::post('upload/avatar', [UploadController::class, 'uploadAvatar']);
+        Route::post('upload/photo', [UploadController::class, 'doUploadPhoto']);
+        Route::post('upload/avatar', [UploadController::class, 'doUploadAvatar']);
     });
 });
