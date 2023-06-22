92a00c0137e2aa2702751db2f1838968b6393261
Marosdee Uma <danya0365@gmail.com>
2023-06-22 21:33:08 +0700

    fix: update user notification

--- app/Http/Controllers/Api/WorkController.php
+++ app/Http/Controllers/Api/WorkController.php
@@ -211,10 +211,13 @@ class WorkController extends Controller
                 $userNotification = UserNotification::whereHasMorph(
                     'notificationable',
                     [Work::class],
-                    function (Builder $query) use ($work) {
+                    function (Builder $query, string $type) use ($work) {
                         $query->where('id', $work->id);
                     }
-                )->where('notification_type', NotificationType::WorkLike())->first();
+                )
+                    ->where('notification_type', NotificationType::WorkLike())
+                    ->whereBelongsTo($user, 'author')
+                    ->first();
                 if ($userNotification) {
                     $details = $userNotification->details;
                     $details['count'] = $details['count'] + 1;
