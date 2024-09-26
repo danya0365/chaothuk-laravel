24efa33bea5a791122c1ae0e4d562bb82a40bb00
Marosdee Uma <danya0365@gmail.com>
2024-09-26 11:12:53 +0700

    refactor: delete laravel 10

--- resources/lang/en/validation.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'unique' => 'The :attribute has already been taken.',
-    'unique_value' => 'The :attribute value :value has already been taken.',
-    'work_code_unique' => 'The :attribute has already been taken.',
-];
